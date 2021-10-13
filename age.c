8860e9a3db49f580a3c400b18097d91ef802e613
unravelly <2801931569@qq.com>
2021-10-13 13:59:36 +0800

    tt

---
+++ age.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+	int age0 = 35 ;
+	int age1 = 0 ;
+	printf ("请输入你的年龄:");
+	scanf ("%d",&age1) ;
+	if (age1<=age0){
+		printf ("年轻是美好的。");
+		
+	}else{
+		printf ("努力多活几年吧。"); 
+	}
+	return 0;
+ } 
